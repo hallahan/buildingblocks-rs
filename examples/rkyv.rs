d4e96f6a6273c1352119fadba9b1e41afa11d6cd
Nick Hallahan <nick@theoutpost.io>
2022-10-24 19:06:16 -0700

    Basic rkyv usage.

--- examples/rkyv.rs
+++ examples/rkyv.rs
@@ -1,7 +1,37 @@
 type Error = Box<dyn std::error::Error>;
 
+use rkyv::{Archive, Deserialize, Serialize};
+// bytecheck can be used to validate your data if you want
+// use bytecheck::CheckBytes;
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+// This will generate a PartialEq impl between our unarchived and archived types
+#[archive(compare(PartialEq))]
+// To use the safe API, you have to derive CheckBytes for the archived type
+#[archive_attr(derive(Debug))]
+struct Test {
+    int: u8,
+    string: String,
+    option: Option<Vec<i32>>,
+}
+
+
 fn run() -> Result<(), Error> {
-    println!("hello world {:?}", 2);
+    
+    let value = Test {
+        int: 42,
+        string: "hello world".to_string(),
+        option: Some(vec![1, 2, 3, 4]),
+    };
+
+    // Serializing is as easy as a single function call
+    let bytes = rkyv::to_bytes::<_, 256>(&value).unwrap();
+
+    // Or you can use the unsafe API for maximum performance
+    let archived = unsafe { rkyv::archived_root::<Test>(&bytes[..]) };
+    assert_eq!(archived, &value);
+
+    println!("archived {:?}", archived);
     Ok(())
 }
 
