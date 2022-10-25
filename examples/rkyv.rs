96b9d1c130b0e02a59666abc1cffd3c9e19affb5
Nick Hallahan <nick@theoutpost.io>
2022-10-24 23:27:36 -0700

    Trying to mutate in rkyv

--- examples/rkyv.rs
+++ examples/rkyv.rs
@@ -1,5 +1,7 @@
 type Error = Box<dyn std::error::Error>;
 
+use std::pin::Pin;
+
 use rkyv::{Archive, Deserialize, Serialize};
 // bytecheck can be used to validate your data if you want
 // use bytecheck::CheckBytes;
@@ -32,8 +34,20 @@ fn run() -> Result<(), Error> {
     assert_eq!(archived, &value);
 
     println!("archived {:?}", archived);
+
+    // Lets see if we can mutate...
+    let mut aligned_vec = rkyv::to_bytes::<_, 256>(&value)?;
+    let bytes_mut = aligned_vec.as_mut_slice();
+    let pin = Pin::<&mut [u8]>::new(bytes_mut);
+    let archived2 = unsafe { rkyv::archived_root_mut::<Test>(pin) };
+    unsafe {
+        let a = archived2.get_unchecked_mut();
+        a.int = 255;
+        println!("mutate {:?}", a);
+    }
+
     Ok(())
-}
+} 
 
 fn main() {
     if let Err(e) = run() {
