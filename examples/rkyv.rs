9bfb99b48aaf969eb75e06ec61e0557f3dfd6992
Nick Hallahan <nick@theoutpost.io>
2022-10-25 10:10:23 -0700

    Memmap a BTreeMap works.

--- examples/rkyv.rs
+++ examples/rkyv.rs
@@ -1,6 +1,6 @@
 type Error = Box<dyn std::error::Error>;
 
-use std::{pin::Pin, fs::OpenOptions};
+use std::{pin::Pin, fs::OpenOptions, io::Write, collections::BTreeMap};
 
 use memmap2::MmapMut;
 use rkyv::{Archive, Deserialize, Serialize};
@@ -73,8 +73,40 @@ fn run() -> Result<(), Error> {
         println!("rkyv_points hex {:02X?}", ap);
     }
 
-    // Make a BTree
 
+
+    // Make a BTree
+    type MyMap = BTreeMap<String, usize>;
+    const FILE_NAME: &'static str = "data/btree.rkyv";
+
+    // build BTreeMap
+    let mut map = MyMap::new();
+
+    for i in 0..100 {
+        map.insert(format!("{i}"), i);
+    }
+
+    // serialize to disk
+    let mut output = OpenOptions::new().create(true).write(true).open(FILE_NAME).unwrap();
+    output.write_all(&rkyv::to_bytes::<_, 1024>(&map).unwrap()).unwrap();
+    drop(output);
+
+    // map into memory
+    let input = OpenOptions::new().read(true).write(true).open(FILE_NAME).unwrap();
+    let mut mmap = unsafe { memmap2::MmapMut::map_mut(&input).unwrap() };
+
+    // check bytes (optional)
+    rkyv::check_archived_root::<MyMap>(&*mmap).unwrap();
+
+    // cast map back out
+    let pinned_bytes = unsafe { Pin::new_unchecked(mmap.as_mut()) };
+    let rkyv_map = unsafe { rkyv::archived_root_mut::<MyMap>(pinned_bytes) };
+
+    // verify
+    for (k, v) in rkyv_map.iter() {
+        println!("k {:?} v {:?}", k, v);
+        assert_eq!(k.parse::<usize>().unwrap(), *v as usize);
+    }
 
 
     Ok(())
